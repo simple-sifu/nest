858b44d45377849ece9f8e339474ebb2e17b0b6e
Tommy Han <tommy_han1@intuit.com>
2022-03-28 21:41:58 -0400

    variables

---
+++ src/typescript/features/annotations/variables.ts
@@ -0,0 +1,8 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+let nothingValue: null = null;
+let nothing: undefined = undefined;
+
+// build in objects
+let now: Date = new Date();
