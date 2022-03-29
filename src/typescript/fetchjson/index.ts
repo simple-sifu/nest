667cd33ecf3a8193ff8b95f9c6678880d876261e
Tommy Han <tommy_han1@intuit.com>
2022-03-28 20:43:04 -0400

    refactor

--- src/typescript/fetchjson/index.ts
+++ src/typescript/fetchjson/index.ts
@@ -9,17 +9,22 @@ interface Todo {
   completed: boolean;
 }
 
+
 axios.get(url).then(response => {
   const todo = response.data as Todo;
 
   const id = todo.id;
   const title = todo.title;
   const finished = todo.completed;
+  logTodo(id, title, finished);
 
+})
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
-  the todo with ID: ${id}
-  Has a title of: ${title}
-  Is it finished? ${finished}
+    the todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
   `
   );
-})
+}
