771d800dacfc6d4004761d898c86d54a231472de
Tommy Han <tommy_han1@intuit.com>
2022-03-28 20:33:18 -0400

    setup axios test

--- src/typescript/fetchjson/index.ts
+++ src/typescript/fetchjson/index.ts
@@ -1,7 +1,25 @@
 import axios from 'axios';
+import { Tracing } from 'trace_events';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  console.log(`
+  the todo with ID: ${ID}
+  Has a title of: ${title}
+  Is it finished? ${finished}
+  `
+  );
 })
