3e808957a758b67dda9ac3d3b779b302cd779d88
Tommy Han <tommy_han1@intuit.com>
2022-03-31 23:32:14 -0400

    variables

--- src/typescript/features/annotations/variables.ts
+++ src/typescript/features/annotations/variables.ts
@@ -33,3 +33,8 @@ const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
 
+// When to use annotations
+// 1.) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates); // {x:10, y:20};
