25d36005de20867cd4be9943516f93afa9d4d4a3
Tommy Han <tommy_han1@intuit.com>
2022-03-31 23:56:36 -0400

    annotation for return

--- src/typescript/features/annotations/functions.ts
+++ src/typescript/features/annotations/functions.ts
@@ -18,3 +18,27 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 }
+
+// can return null or undefined
+const logger = (message: string): void => {
+  console.log(message);
+}
+
+// this is very rare.
+const throwError = (message: string): never => {
+  throw new Error(message);
+}
+
+const throwError0 = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+  return message;
+}
+
+const throwError1 = (message: string): void => {
+  if (!message){
+    throw new Error(message);
+  }
+}
+
