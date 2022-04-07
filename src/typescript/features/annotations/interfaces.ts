033ddb8e9efd01dd3ccc70a4252fd49c7868b871
Tommy Han <tommy_han1@intuit.com>
2022-04-07 10:07:40 -0400

    interfaces

--- src/typescript/features/annotations/interfaces.ts
+++ src/typescript/features/annotations/interfaces.ts
@@ -11,3 +11,17 @@ const printVehicle = (vehicle: {name: string; year: number; broken: boolean}): v
 }
 
 printVehicle(oldCivic);
+
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+const printVehicle1 = (vehicle: Vehicle): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken? ${vehicle.broken}`);
+}
+
+printVehicle1(oldCivic);
